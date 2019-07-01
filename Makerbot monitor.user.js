f22894c66e00950ddd52d74650c621b59e9ac28b
Steve Koehler <steve.koehler@gmail.com>
2019-07-01 11:37:24 -0500

    Initial

---
+++ Makerbot monitor.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         Makerbot monitor
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Isolate the makerbot print monitor
+// @author       nexusgx
+// @match        https://my.makerbot.com/printers
+// @grant        none
+// ==/UserScript==
+
+(function() {
+
+    setTimeout(addBtn,1000);
+    // Your code here...
+})();
+var elemsToHide=".nav-spacer,.site-header,.makerbot-footer,.separator,.support-note,.printers .page-title,#start-print,#accounts .row .col-md-3,#content .printers .col-md-6:first-child";
+
+function addBtn(){
+    $('body').prepend(
+        '<button id="monitorprinter" style="width: 100%;font-size: 20px;color: #fff;background-color: #a22;border: 1px solid #efefef;position: absolute;top: 0;left: 0;z-index:10000;">MONITOR PRINTER</button>'+
+        '<button id="hidemonitor" style="width: 100%;font-size: 20px;color: #fff;background-color: #2a2;border: 1px solid #efefef;position: absolute;top: 0;left: 0; display:none;z-index:10000;">HIDE MONITOR</button>'
+    );
+    $('#monitorprinter').on('click',function(){
+        $(elemsToHide).hide();
+        $('object').attr('style','width:100%');
+        $("#accounts .row .col-md-8").addClass('col-md-12'); //widen the printer column
+        $('#content .printers .col-md-6:last-child').addClass('col-md-12'); //widen the printer display
+        $('#monitorprinter').hide();
+        $('#hidemonitor').show();
+
+    });
+    $('#hidemonitor').on('click',function(){ //revert everything
+        $(elemsToHide).show();
+        $('object').attr('style','');
+        $("#accounts .row .col-md-8").removeClass('col-md-12');
+        $('#content .printers .col-md-6:last-child').removeClass('col-md-12');
+        $('#monitorprinter').show();
+        $('#hidemonitor').hide();
+    });
+}
